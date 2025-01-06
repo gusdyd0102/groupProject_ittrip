c72e85a2fcd729818a4c0b80f84bb98a98573209
gusdyd0102 <gusdyd0102@gmail.com>
2025-01-06 16:24:08 +0900

    g

--- react/src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from '../img/Logo/logo.svg'
-
-const Logo = () => {
-    return(
-        <img src={logo} alt="logo" style={{width:'100%'}}/>
-    )
-}
-export default Logo;
